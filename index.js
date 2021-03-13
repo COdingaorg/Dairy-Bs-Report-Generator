87ae3101c00875bbd9c386b61834c6da8941b76f
COdingaorg <calemasanga@gmail.com>
2021-03-13 10:04:18 +0300

    Add sheds Array objects,formula that prints out production per shed which is a random number

---
+++ index.js
@@ -0,0 +1,27 @@
+var sheds=[
+  Shed_A={
+    name:"Shed-A",
+    litresProduced:Math.floor(Math.random()*1000),
+  },
+  Shed_B={
+    name:"Shed-B",
+    litresProduced:Math.floor(Math.random()*1000),
+  }, 
+  Shed_C={
+    name:"Shed-C",
+    litresProduced:Math.floor(Math.random()*1000),
+  }, 
+  Shed_D={
+    name:"Shed-D",
+    litresProduced:Math.floor(Math.random()*1000),
+  }, 
+  Shed_E={
+    name:"Shed-E",
+    litresProduced:Math.floor(Math.random()*1000),
+  }];
+ for (var i=0; i<sheds.length; i++){
+   var i;
+  /*return ("Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"Litres");*/
+  console.log("Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"Litres");
+ };
+ 
