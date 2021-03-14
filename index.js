e44931e8993a92fc184a8c15db4ae529b084b2c1
COdingaorg <calemasanga@gmail.com>
2021-03-14 18:47:50 +0300

    add button to html, function to js that tries to calculate weekly and yearly income

--- index.js
+++ index.js
@@ -19,5 +19,27 @@ var sheds=[//creates 5 sheds
     name:"Shed-E",
     litresProduced:Math.floor(Math.random()*1000),
   }];
-  
- document.getElementById("reportA").innerHTML=sheds[2].name;
+    for (var i=0; i<sheds.length; i++){
+      try{throw i}
+      catch(ii) {
+    document.getElementById("reportA").innerHTML="Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"litres";
+      }
+  };
+  var totalMilkProd=sheds[0].litresProduced+sheds[1].litresProduced+sheds[2].litresProduced+sheds[3].litresProduced+sheds[4].litresProduced+" "+"Litres";
+  document.getElementById("totalPerDay").innerHTML=totalMilkProd;
+ /*document.getElementById("test").innerHTML=sheds[2].name;
+ for (var i=0; i<sheds.length; i++){
+   //document.getElementById("reportA").innerHTML=
+   console.log("Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"litres");
+   if(i<sheds.length){
+   document.getElementById("reportA").innerHTML="Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"litres"};
+ };*/
+ function calcTime(ppltr1, time){
+   if (time="week"){
+     document.getElementById("pricePerLitre").innerHTML="Your weekly income is"+week*ppltr1*totalMilkProd;
+   }
+   else if(time="year"){
+     document.getElementById("pricePerLitre").innerHTML="Your Yearly income is"+year*ppltr1*totalMilkProd;
+   };
+ };
+ calcTime(parseInt(document.getElementById("ppltr1").value),parseFloat(document.getElementById("time").value));
