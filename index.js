597cd4439488355de96a74bb7b08794e8a4ad80b
COdingaorg <calemasanga@gmail.com>
2021-03-15 06:44:09 +0300

    Adds individual sheds id to html, prints value for the sheds in javascript

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
-var sheds=[//creates 5 sheds
-  Shed_A={
-    name:"Shed-A",
-    litresProduced:Math.floor(Math.random()*1000),
+var litresProduced;
+var sheds=[
+  shedA={
+    name:"shed_A",
+    litresProduced:Math.floor(Math.random()*1000)
   },
-  Shed_B={
-    name:"Shed-B",
-    litresProduced:Math.floor(Math.random()*1000),
-  }, 
-  Shed_C={
-    name:"Shed-C",
-    litresProduced:Math.floor(Math.random()*1000),
-  }, 
-  Shed_D={
-    name:"Shed-D",
-    litresProduced:Math.floor(Math.random()*1000),
-  }, 
-  Shed_E={
-    name:"Shed-E",
-    litresProduced:Math.floor(Math.random()*1000),
-  }];
-    for (var i=0; i<sheds.length; i++){
-      try{throw i}
-      catch(ii) {
-    document.getElementById("reportA").innerHTML="Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"litres";
-      }
+  shedB={
+    name:"shed_B",
+    litresProduced:Math.floor(Math.random()*1000)
+  },
+  shedC={
+    name:"shed_C",
+    litresProduced:Math.floor(Math.random()*1000)
+  },
+  shedD={
+    name:"shed_D",
+    litresProduced:Math.floor(Math.random()*1000)
+  },
+  shedE={
+    name:"shed_E",
+    litresProduced:Math.floor(Math.random()*1000)
+  },
+];
+ /* sheds.forEach(litresProduced => {
+    for(i=0; 1<sheds.length; i++){
+    //document.getElementById("reportA").innerHTML="Your production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"Litres";
   };
-  var totalMilkProd=sheds[0].litresProduced+sheds[1].litresProduced+sheds[2].litresProduced+sheds[3].litresProduced+sheds[4].litresProduced+" "+"Litres";
-  document.getElementById("totalPerDay").innerHTML=totalMilkProd;
- /*document.getElementById("test").innerHTML=sheds[2].name;
- for (var i=0; i<sheds.length; i++){
-   //document.getElementById("reportA").innerHTML=
-   console.log("Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"litres");
-   if(i<sheds.length){
-   document.getElementById("reportA").innerHTML="Your Production in"+" "+sheds[i].name+" "+"is"+" "+sheds[i].litresProduced+" "+"litres"};
- };*/
- function calcTime(ppltr1, time){
-   var document.getElementById("ppltr1").value;
-   var document.getElementById("time").value;
-   if (time="week"){
-     document.getElementById("pricePerLitre").innerHTML="Your weekly income is"+week*ppltr1*totalMilkProd;
-   }
-   else if(time="year"){
-     document.getElementById("pricePerLitre").innerHTML="Your Yearly income is"+year*ppltr1*totalMilkProd;
-   };
- };
- var function submission(){
-   calcTime(parseInt(document.getElementById("ppltr1").value),parseFloat(document.getElementById("time").value));
+});*/
+var totalMilkProd=sheds[0].litresProduced+sheds[1].litresProduced+sheds[2].litresProduced+sheds[3].litresProduced+sheds[4].litresProduced;
+document.getElementById("totalPerDay").innerHTML="Your Total Milk Production is"+" "+totalMilkProd+"Litres";
+document.getElementById("shed1").innerHTML = "Your production in"+" "+sheds[0].name+" "+"is"+" "+sheds[0].litresProduced+" "+"Litres";
+document.getElementById("shed2").innerHTML = "Your production in"+" "+sheds[1].name+" "+"is"+" "+sheds[0].litresProduced+" "+"Litres";
+document.getElementById("shed3").innerHTML = "Your production in"+" "+sheds[2].name+" "+"is"+" "+sheds[0].litresProduced+" "+"Litres";
+document.getElementById("shed4").innerHTML = "Your production in"+" "+sheds[3].name+" "+"is"+" "+sheds[0].litresProduced+" "+"Litres";
+document.getElementById("shed5").innerHTML = "Your production in"+" "+sheds[4].name+" "+"is"+" "+sheds[0].litresProduced+" "+"Litres";
+document.getElementById("ppltr1").value
+//function that prints income.
