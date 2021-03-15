ae4231038036c0f101c09c4b9f8b925b225d0941
COdingaorg <calemasanga@gmail.com>
2021-03-15 07:50:41 +0300

    Add formula that prints income for each month and displays them on document

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function report(sellingPrice, time) {
   var result=sellingPrice*time*totalMilkProd;
   var week=7;
   var year=366;
+  var sellingPrice=45;
     if (time=week) {
       result=sellingPrice*week*totalMilkProd;
       document.getElementById("weekRevenue").innerHTML="Your Weekly income is Kshs"+" "+result;
@@ -54,3 +55,68 @@ function report(sellingPrice, time) {
     };
 };
 //report(document.getElementById("ppltr1").value,document.getElementById("time").value);
+//creates object months for leap year
+var sellingPrice=45;
+ var month1={
+  name:"January",
+  days:31
+ };
+ var month2={
+   name:"February",
+   days:29
+ };
+ var month3={
+  name:"March",
+  days:31
+};
+var month4={
+  name:"April",
+  days:30
+};
+var month5={
+  name:"May",
+  days:31
+};
+var month6={
+  name:"June",
+  days:30
+};
+var month7={
+  name:"July",
+  days:31
+};
+var month8={
+  name:"August",
+  days:31
+};
+var month9={
+  name:"September",
+  days:30
+};
+var month10={
+  name:"October",
+  days:31
+};
+var month11={
+  name:"November",
+  days:30
+};
+var month12={
+  name:"December",
+  days:31
+};
+document.getElementById("january").innerHTML="Your income for"+" "+month1.name+" "+"is"+" "+month1.days*totalMilkProd*sellingPrice;
+document.getElementById("february").innerHTML="Your income for"+" "+month2.name+" "+"is"+" "+month2.days*totalMilkProd*sellingPrice;
+document.getElementById("march").innerHTML="Your income for"+" "+month3.name+" "+"is"+" "+month3.days*totalMilkProd*sellingPrice;
+document.getElementById("april").innerHTML="Your income for"+" "+month4.name+" "+"is"+" "+month4.days*totalMilkProd*sellingPrice;
+document.getElementById("may").innerHTML="Your income for"+" "+month5.name+" "+"is"+" "+month5.days*totalMilkProd*sellingPrice;
+document.getElementById("june").innerHTML="Your income for"+" "+month6.name+" "+"is"+" "+month6.days*totalMilkProd*sellingPrice;
+document.getElementById("july").innerHTML="Your income for"+" "+month7.name+" "+"is"+" "+month7.days*totalMilkProd*sellingPrice;
+document.getElementById("august").innerHTML="Your income for"+" "+month8.name+" "+"is"+" "+month8.days*totalMilkProd*sellingPrice;
+document.getElementById("september").innerHTML="Your income for"+" "+month9.name+" "+"is"+" "+month9.days*totalMilkProd*sellingPrice;
+document.getElementById("october").innerHTML="Your income for"+" "+month10.name+" "+"is"+" "+month10.days*totalMilkProd*sellingPrice;
+document.getElementById("november").innerHTML="Your income for"+" "+month11.name+" "+"is"+" "+month11.days*totalMilkProd*sellingPrice;
+document.getElementById("december").innerHTML="Your income for"+" "+month12.name+" "+"is"+" "+month12.days*totalMilkProd*sellingPrice;
+var totalYearIncome=(month1.days*totalMilkProd)+(month2.days*totalMilkProd*sellingPrice)+(month3.days*totalMilkProd*sellingPrice)+(month4.days*totalMilkProd*sellingPrice)+(month5.days*totalMilkProd*sellingPrice)+(month6.days*totalMilkProd*sellingPrice)+(month7.days*totalMilkProd*sellingPrice)+(month8.days*totalMilkProd*sellingPrice)+(month9.days*totalMilkProd*sellingPrice)+(month10.days*totalMilkProd*sellingPrice)+(month11.days*totalMilkProd*sellingPrice)+(month12.days*totalMilkProd*sellingPrice);
+document.getElementById("totalYear").innerHTML="Your total income for the year is"+totalYearIncome;
+
